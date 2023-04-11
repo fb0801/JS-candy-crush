00f037e05adbaec95e379c03cd4dbffc6ab9f599
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2023-04-11 17:13:39 +0100

    candy

--- candyCrush/app.js
+++ candyCrush/app.js
@@ -11,9 +11,52 @@ const candyColors = [
     function createBoard(){
         for(let i = 0;i < width*width; i++) {
         const square = document.createElement('div')
+        square.setAttribute('draggable', true)  //make items draggable
+        square.setAttribute('id', i)
+        let randomColor = Math.floor(Math.random() * candyColors.length)
+        square.style.backgroundColor = candyColors[randomColor]
         grid.appendChild(square)
         squares.push(square)
         }
     }
+
+let colorBeingDragged
+let colorBeingReplaced
+
+
     createBoard()
+//drag candy
+squares.forEach(square => square.addEventListener('dragstart', dragStart))
+squares.forEach(square => square.addEventListener('dragend', dragend))
+squares.forEach(square => square.addEventListener('dragover', dragOver))
+squares.forEach(square => square.addEventListener('dragenter', dragEnter))
+squares.forEach(square => square.addEventListener('dragLeave', dragLeave))
+squares.forEach(square => square.addEventListener('drop', drop))
+
+
+function dragStart(){
+    
+
+}
+
+function dragend(){
+    
+}
+
+function dragOver(){
+    
+}
+
+function dragEnter(){
+    
+}
+
+function dragLeave(){
+    
+}
+
+
+
+
+
 })
